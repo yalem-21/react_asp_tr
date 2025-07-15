e8eee8ee0753a057dd4fe291e5ddc651feda1a3a
yalem-21 <yalemzewud32@gmail.com>
2025-07-15 17:21:58 +0300

    third try

--- react_asp_tr.client/src/Ecx.jsx
+++ react_asp_tr.client/src/Ecx.jsx
@@ -1,19 +1,42 @@
 import './Style/Ecx.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Images from './assets/images.png'
+import Images from './assets/images.jpg'
+import './index.css';
 function Ecx() {
     return (
         <>
             <div>
                 <div className="navbar">
                     <div className="side1">
-                        <img src={Images} alt="X ECX Image" height="40px" />
+                        <img src={Images} alt="X ECX Image" height="40px" /><p>ECX  board</p>
                     </div>
-                    <div className="side2"><p className="tittle">Project name</p></div>
-                   
+                    <div className="side2">
+                        <div>
+                            <div>
+                                <p>hhdhsjv</p>
+                            </div>
+                            <div>
+                                <p>hhdhsjv</p>
+                            </div>
+                        </div>
+                       
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                     
                 </div>
                 <p><i className="bi bi-0-circle-fill"></i>fgyhj <i className="bi bi-0-circle-fill"></i></p>
-            </div>   
+            </div> 
+            <h1 className=" underline text-blue">
+                Hello world!
+            </h1>
+            <div className="flex flex-row text-3xl  underline">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+            </div>
         </>
   );
 }
---
+++ react_asp_tr.client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Scan your React components
+        "./public/index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
