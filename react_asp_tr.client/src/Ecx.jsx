9bbdcab6371c616903a383c3a45fa3cc9703f760
yalem-21 <yalemzewud32@gmail.com>
2025-07-16 09:38:03 +0300

    next day commit

--- react_asp_tr.client/src/Ecx.jsx
+++ react_asp_tr.client/src/Ecx.jsx
@@ -1,44 +1,51 @@
-import './Style/Ecx.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Images from './assets/images.jpg'
-import './index.css';
-function Ecx() {
+import Image from './assets/images.jpg'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavScrollExample() {
     return (
-        <>
-            <div>
-                <div className="navbar">
-                    <div className="side1">
-                        <img src={Images} alt="X ECX Image" height="40px" /><p>ECX  board</p>
-                    </div>
-                    <div className="side2">
-                        <div>
-                            <div>
-                                <p>hhdhsjv</p>
-                            </div>
-                            <div>
-                                <p>hhdhsjv</p>
-                            </div>
-                        </div>
-                       
-                        <div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    </div>
-                     
-                </div>
-                <p><i className="bi bi-0-circle-fill"></i>fgyhj <i className="bi bi-0-circle-fill"></i></p>
-            </div> 
-            <h1 className=" underline text-blue">
-                Hello world!
-            </h1>
-            <div className="flex flex-row text-3xl  underline">
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-            </div>
-        </>
-  );
+        <Navbar expand="sm" style={{ backgroundColor:'#302f2b'} }>
+            <Container fluid>
+                <Navbar.Brand href="#" style={{ color: 'white', fontWeight: 'bold' }}> <img src={Image} width='30px' height='30px' />
+                    <span style={{ marginLeft: '7px' }}>ECX</span>
+                </Navbar.Brand>
+                <Navbar.Toggle style={{ color: 'white', borderColor: 'white', backgroundColor:'white' }} aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{
+                            maxHeight: '200px',
+                            color:'white'
+                        }}
+                        /*navbarScroll*/
+                    >
+                        <Nav.Link style={{ color: 'white', fontWeight: 'bold' }} href="#action1">Home</Nav.Link>
+                        <Nav.Link style={{ color: 'white', fontWeight: 'bold' }} href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Something else here
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                      
+                    </Nav>
+                    <Nav className="d-flex">
+                        <Nav.Link style={{ color: 'white', fontWeight: 'bold' }} href="#action1">Home</Nav.Link>
+                        <Nav.Link style={{ color: 'white', fontWeight: 'bold' }} href="#action2">Link</Nav.Link>
+                        <Button style={{ color: 'white', fontWeight: 'bold' }} variant="outline-success">Search</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Ecx;
+export default NavScrollExample; 
